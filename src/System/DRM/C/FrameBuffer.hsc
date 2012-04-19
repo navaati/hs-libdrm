d1460d24c6ecd2bff0de90dac6b0864280086dba
Léo Gillot-Lamure <leo.gillot@navaati.net>
2012-04-19 21:09:22 +0100

    Switched FrameBuffer to bindings-dsl

---
+++ src/System/DRM/C/FrameBuffer.hsc
@@ -0,0 +1,26 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+#include <bindings.dsl.h>
+
+#include<stdint.h>
+#include<xf86drmMode.h>
+
+module System.DRM.C.FrameBuffer where
+#strict_import
+import System.DRM.Types
+import System.Posix.Types
+
+#starttype drmModeFB
+#field fb_id, Word32
+#field width, Word32
+#field height, Word32
+#field pitch, Word32
+#field bpp, Word32
+#field depth, Word32
+#field handle, Word32
+#stoptype
+
+#ccall drmModeGetFB, Drm → FbId drm → IO (Ptr <drmModeFB>)
+#ccall drmModeFreeFB, Ptr <drmModeFB> → IO ()
+#ccall drmModeAddFB, Drm → Width → Height → Depth → BPP → Pitch → BOHandle drm → Ptr (FbId drm) → IO CInt
---
+++ src/System/DRM/FrameBuffer.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module System.DRM.FrameBuffer (Fb(..),getFb,addFb) where
+
+import Foreign
+import Foreign.C.Error
+import Data.Reflection
+import Data.Proxy
+
+import System.DRM.C.FrameBuffer
+import System.DRM.Types
+import System.DRM.BufferObject
+
+data Fb drm = Fb
+              { fbId ∷ FbId drm
+              , fbRes ∷ (Width,Height)
+              , fbPitch ∷ Pitch
+              , fbBPP, fbDepth ∷ Word32
+              , fbHandle ∷ BOHandle drm
+              } deriving (Show)
+
+cToFb ∷ C'drmModeFB → Fb drm
+cToFb (C'drmModeFB{..}) =
+  Fb
+  (FbId c'drmModeFB'fb_id)
+  (c'drmModeFB'width,c'drmModeFB'height)
+  c'drmModeFB'pitch
+  c'drmModeFB'bpp
+  c'drmModeFB'depth
+  (BOHandle c'drmModeFB'handle)
+
+getFb ∷ (RDrm drm) ⇒
+  FbId drm → IO (Fb drm)
+getFb fId = do
+  ptr ← throwErrnoIfNull "drmModeGetFB" $
+         applyDrm c'drmModeGetFB fId
+  fb ← peek ptr
+  c'drmModeFreeFB ptr
+  return $ cToFb fb
+
+addFb ∷ ∀drm bo. (RDrm drm, ImageBO bo drm) ⇒ bo → IO (FbId drm)
+addFb bo = alloca $ \fIdPtr → do
+  let (w,h) = boRes bo
+  throwErrnoIfMinus1_ "drmModeAddFB" $ c'drmModeAddFB
+    (reflect (Proxy ∷ Proxy drm)) w h (boDepth bo)
+    (boBPP bo) (boPitch bo) (boHandle bo) fIdPtr
+  peek fIdPtr
