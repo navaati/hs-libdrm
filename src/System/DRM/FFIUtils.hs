47ddccd71800f5a7768875a2724f4fa686ccc635
Léo Gillot-Lamure <leo.gillot@navaati.net>
2012-04-20 18:39:39 +0100

    Remove now useless functions from FFIUtils

--- src/System/DRM/FFIUtils.hs
+++ src/System/DRM/FFIUtils.hs
@@ -1,20 +1,10 @@
 module System.DRM.FFIUtils where
 
 import Prelude.Unicode
-import Foreign
-import Control.Monad
+import Data.Bits
 import Data.Maybe(fromJust)
 
-lPeekArray ∷ Storable β ⇒
-              Ptr α → (Ptr α → IO Int) → (Ptr α → IO (Ptr β)) →
-              IO [β]
-lPeekArray ptr count arr = join $ liftM2 peekArray (count ptr) (arr ptr)
 
-peekEnum ∷ (Eq k, Storable k) ⇒ [(k,α)] →
-            Ptr α → IO α
-peekEnum table ptr = do
-  cint ← peek $ castPtr ptr
-  return $ fromJust $ lookup cint table
 
 cToFlags ∷ (Bits f, Eq e) ⇒ [(e,f)] → f → [e]
 cToFlags table cword = filter (isFlagSet ∘ eToF) $ map fst table
